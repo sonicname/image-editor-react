b09641416df8c91a259966d7b53b23d827445179
sonicname <phamanhduc2k2@mail.com>
2022-12-02 12:24:30 +0700

    update toogle show sidebar

--- client/src/components/sidebar/Sidebar.tsx
+++ client/src/components/sidebar/Sidebar.tsx
@@ -1,18 +1,35 @@
+import classNames from 'classnames';
+
 import SideBarItem from './SideBarItem';
+import IconDown from '../icons/IconDown';
+
+import useToggle from '../../hooks/useToggle';
 
 const Sidebar = () => {
+  const { isToggled, toggle } = useToggle();
   return (
-    <div className='py-5 flex flex-col min-h-screen text-black shadow-md'>
-      <h2 className='text-center font-semibold mb-2'>Editor Tools</h2>
-      <SideBarItem content='Brightness' />
-      <SideBarItem content='Rotate & Flip' />
-      <SideBarItem content='Blur' />
-      <SideBarItem content='Saturate' />
-      <SideBarItem content='Contrast' />
-      <SideBarItem content='RGB' />
-      <SideBarItem content='Grayscale' />
-      <SideBarItem content='Opacity' />
-      <SideBarItem content='Filter' />
+    <div className='relative'>
+      <div
+        className={classNames(
+          'py-5 flex flex-col min-h-screen text-black shadow-md overflow-hidden',
+          isToggled ? 'w-0 invisible' : 'w-auto visible',
+        )}
+      >
+        <h2 className='text-center font-semibold mb-2'>Editor Tools</h2>
+        <SideBarItem content='Brightness' />
+        <SideBarItem content='Rotate & Flip' />
+        <SideBarItem content='Blur' />
+        <SideBarItem content='Saturate' />
+        <SideBarItem content='Contrast' />
+        <SideBarItem content='RGB' />
+        <SideBarItem content='Grayscale' />
+        <SideBarItem content='Opacity' />
+        <SideBarItem content='Filter' />
+      </div>
+
+      <div className='absolute top-0 left-full m-1 p-2 bg-white rounded-sm' onClick={toggle}>
+        <IconDown className={classNames(isToggled ? 'rotate-90' : '-rotate-90', 'duration-100')} />
+      </div>
     </div>
   );
 };
