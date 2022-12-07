5df1e21dde77d67889e380ddd478dd69492188da
sonicname <phamanhduc2k2@mail.com>
2022-12-07 10:02:42 +0700

    update rotate, flip function

--- client/src/components/sidebar/SidebarRight.tsx
+++ client/src/components/sidebar/SidebarRight.tsx
@@ -2,24 +2,62 @@ import { memo } from 'react';
 
 import SideBarItem from './SideBarItem';
 
+import useEditorStore from '../../store/useEditorStore';
+import classNames from 'classnames';
+
 const SidebarRight = () => {
+  const {
+    isRotated,
+    isFlipped,
+    isReverseFlipped,
+    isReverseRotated,
+    touchedFlipped,
+    touchedRotate,
+    touchedReverseRotate,
+    touchedReverseFlip,
+  } = useEditorStore();
+
   return (
     <div className={'py-2 flex flex-col h-screen w-60 overflow-y-scroll text-black shadow-md'}>
       <SideBarItem content='Rotate & Flip'>
         <div className='p-2 lg:p-4 grid grid-cols-2 gap-2 lg:gap-4'>
-          <button className='p-2 flex items-center justify-center shadow-md rounded-md border border-gray-300 active:scale-90 duration-75'>
+          <button
+            className={classNames(
+              'p-2 flex items-center justify-center shadow-md rounded-md border border-gray-300 active:scale-90 duration-75',
+              isRotated && 'bg-green-500 text-white',
+            )}
+            onClick={touchedRotate}
+          >
             <img className='w-6 h-6 object-cover' src='/rotate.png' alt='rorate' />
           </button>
 
-          <button className='p-2 flex items-center justify-center shadow-md rounded-md border border-gray-300 active:scale-90 duration-75'>
+          <button
+            className={classNames(
+              'p-2 flex items-center justify-center shadow-md rounded-md border border-gray-300 active:scale-90 duration-75',
+              isReverseRotated && 'bg-green-500 text-white',
+            )}
+            onClick={touchedReverseRotate}
+          >
             <img className='w-6 h-6 object-cover scale-x-[-1]' src='/rotate.png' alt='rorate' />
           </button>
 
-          <button className='p-2 flex items-center justify-center shadow-md rounded-md border border-gray-300 active:scale-90 duration-75'>
+          <button
+            className={classNames(
+              'p-2 flex items-center justify-center shadow-md rounded-md border border-gray-300 active:scale-90 duration-75',
+              isFlipped && 'bg-green-500 text-white',
+            )}
+            onClick={touchedFlipped}
+          >
             <img className='w-6 h-6 object-cover' src='/flip.png' alt='rorate' />
           </button>
 
-          <button className='p-2 flex items-center justify-center shadow-md rounded-md border border-gray-300 active:scale-90 duration-75'>
+          <button
+            className={classNames(
+              'p-2 flex items-center justify-center shadow-md rounded-md border border-gray-300 active:scale-90 duration-75',
+              isReverseFlipped && 'bg-green-500 text-white',
+            )}
+            onClick={touchedReverseFlip}
+          >
             <img className='w-6 h-6 object-cover -rotate-90' src='/flip.png' alt='rorate' />
           </button>
         </div>
